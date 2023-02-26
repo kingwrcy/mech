afb5e7fddf56dc755a47a1fb1a0816c688548e20
Steven Penny <rpooak@riseup.net>
2023-02-25 20:56:20 -0600

    deleted:    blog/widevine/pssh.go

--- blog/widevine/widevine_test.go
+++ blog/widevine/widevine_test.go
@@ -4,7 +4,7 @@ import (
    "2a.pages.dev/mech/roku"
    "bytes"
    "encoding/base64"
-   "fmt"
+   "encoding/hex"
    "io"
    "net/http"
    "os"
@@ -27,18 +27,17 @@ var tests = map[key]struct {
    playback_ID string
    init_data string
 } {
-   // therokuchannel.roku.com/watch/2b3166271d83569c81d41030e9ba7fb0
-   {movie, 500}: {playback_ID: "2b3166271d83569c81d41030e9ba7fb0"},
-   // therokuchannel.roku.com/watch/597a64a4a25c5bf6af4a8c7053049a6f
-   {movie, 200}: {
-      key: "13d7c7cf295444944b627ef0ad2c1b3c",
-      playback_ID: "597a64a4a25c5bf6af4a8c7053049a6f",
-   },
    // therokuchannel.roku.com/watch/105c41ea75775968b670fbb26978ed76
    {episode, 500}: {
       init_data: "AAAAQ3Bzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAACMIARIQvfpNbNs5cC5baB+QYX+afhoKaW50ZXJ0cnVzdCIBKg==",
       key: "e258b67d75420066c8424bd142f84565",
       playback_ID: "105c41ea75775968b670fbb26978ed76",
+   },
+   // therokuchannel.roku.com/watch/12cdc6139bb858f4a597a1aa51a1e37d
+   {episode, 200}: {
+      init_data: "AAAAQ3Bzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAACMIARIQmKI1QsJl+5Fx3Kpnu/oGTBoKaW50ZXJ0cnVzdCIBKg==",
+      key: "17e71e939c2a541142112c376cf73cc4",
+      playback_ID: "12cdc6139bb858f4a597a1aa51a1e37d",
    },
 }
 
@@ -56,50 +55,50 @@ func Test_Post(t *testing.T) {
       t.Fatal(err)
    }
    for _, test := range tests {
-      if test.init_data != "" {
-         init_data, err := base64.StdEncoding.DecodeString(test.init_data)
-         if err != nil {
-            t.Fatal(err)
-         }
-         mod, err := NewCDM(private_key, client_ID, init_data)
-         if err != nil {
-            t.Fatal(err)
-         }
-         req_body, err := mod.GetLicenseRequest()
-         if err != nil {
-            t.Fatal(err)
-         }
-         site, err := roku.New_Cross_Site()
-         if err != nil {
-            t.Fatal(err)
-         }
-         play, err := site.Playback(test.playback_ID)
-         if err != nil {
-            t.Fatal(err)
-         }
-         req, err := http.NewRequest(
-            "POST", play.DRM.Widevine.License_Server, bytes.NewReader(req_body),
-         )
-         if err != nil {
-            t.Fatal(err)
-         }
-         res, err := new(http.Transport).RoundTrip(req)
-         if err != nil {
-            t.Fatal(err)
-         }
-         res_body, err := io.ReadAll(res.Body)
-         if err != nil {
-            t.Fatal(err)
-         }
-         if err := res.Body.Close(); err != nil {
-            t.Fatal(err)
-         }
-         keys, err := mod.GetLicenseKeys(req_body, res_body)
-         if err != nil {
-            t.Fatal(err)
-         }
-         fmt.Println(keys)
-         time.Sleep(time.Second)
+      init_data, err := base64.StdEncoding.DecodeString(test.init_data)
+      if err != nil {
+         t.Fatal(err)
       }
+      mod, err := NewCDM(private_key, client_ID, init_data)
+      if err != nil {
+         t.Fatal(err)
+      }
+      req_body, err := mod.GetLicenseRequest()
+      if err != nil {
+         t.Fatal(err)
+      }
+      site, err := roku.New_Cross_Site()
+      if err != nil {
+         t.Fatal(err)
+      }
+      play, err := site.Playback(test.playback_ID)
+      if err != nil {
+         t.Fatal(err)
+      }
+      req, err := http.NewRequest(
+         "POST", play.DRM.Widevine.License_Server, bytes.NewReader(req_body),
+      )
+      if err != nil {
+         t.Fatal(err)
+      }
+      res, err := new(http.Transport).RoundTrip(req)
+      if err != nil {
+         t.Fatal(err)
+      }
+      res_body, err := io.ReadAll(res.Body)
+      if err != nil {
+         t.Fatal(err)
+      }
+      if err := res.Body.Close(); err != nil {
+         t.Fatal(err)
+      }
+      keys, err := mod.GetLicenseKeys(req_body, res_body)
+      if err != nil {
+         t.Fatal(err)
+      }
+      if key := hex.EncodeToString(keys[0].Value); key != test.key {
+         t.Fatal(key)
+      }
+      time.Sleep(time.Second)
    }
 }
