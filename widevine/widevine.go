30e2011547f8fc0452ad9a3862c759185ba57e1f
Steven Penny <rpooak@riseup.net>
2023-02-25 22:44:28 -0600

    deleted:    widevine/post.go

--- widevine/widevine.go
+++ widevine/widevine.go
@@ -3,13 +3,47 @@ package widevine
 import (
    "2a.pages.dev/rosso/http"
    "2a.pages.dev/rosso/protobuf"
+   "bytes"
+   "crypto"
+   "crypto/aes"
+   "crypto/cipher"
    "crypto/rsa"
+   "crypto/sha1"
    "crypto/x509"
-   "encoding/base64"
    "encoding/hex"
    "encoding/pem"
+   "github.com/chmike/cmac-go"
+   "io"
    "strings"
 )
+
+var Client = http.Default_Client
+
+func Key_ID(raw string) ([]byte, error) {
+   raw = strings.ReplaceAll(raw, "-", "")
+   return hex.DecodeString(raw)
+}
+
+func unpad(buf []byte) []byte {
+   if len(buf) >= 1 {
+      pad := buf[len(buf)-1]
+      if len(buf) >= int(pad) {
+         buf = buf[:len(buf)-int(pad)]
+      }
+   }
+   return buf
+}
+
+type Container struct {
+   Key []byte
+   Type uint64
+}
+
+func (c Container) String() string {
+   return hex.EncodeToString(c.Key)
+}
+
+type Containers []Container
 
 func (c Containers) Content() *Container {
    for _, container := range c {
@@ -20,9 +54,9 @@ func (c Containers) Content() *Container {
    return nil
 }
 
-type Container struct {
-   Key []byte
-   Type uint64
+type Module struct {
+   license_request []byte
+   private_key *rsa.PrivateKey
 }
 
 func New_Module(private_key, client_ID, key_ID []byte) (*Module, error) {
@@ -48,52 +82,122 @@ func New_Module(private_key, client_ID, key_ID []byte) (*Module, error) {
    return &mod, nil
 }
 
+func (m Module) Post(post Poster) (Containers, error) {
+   signed_request, err := m.signed_request()
+   if err != nil {
+      return nil, err
+   }
+   body, err := post.Request_Body(signed_request)
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", post.Request_URL(), bytes.NewReader(body),
+   )
+   if err != nil {
+      return nil, err
+   }
+   if head := post.Request_Header(); head != nil {
+      req.Header = head
+   }
+   res, err := Client.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   body, err = io.ReadAll(res.Body)
+   if err != nil {
+      return nil, err
+   }
+   body, err = post.Response_Body(body)
+   if err != nil {
+      return nil, err
+   }
+   return m.signed_response(body)
+}
+
+func (m Module) signed_request() ([]byte, error) {
+   digest := sha1.Sum(m.license_request)
+   signature, err := rsa.SignPSS(
+      no_operation{},
+      m.private_key,
+      crypto.SHA1,
+      digest[:],
+      &rsa.PSSOptions{SaltLength: rsa.PSSSaltLengthEqualsHash},
+   )
+   if err != nil {
+      return nil, err
+   }
+   signed_request := protobuf.Message{
+      2: protobuf.Bytes(m.license_request),
+      3: protobuf.Bytes(signature),
+   }
+   return signed_request.Marshal(), nil
+}
+
+func (m Module) signed_response(response []byte) (Containers, error) {
+   // key
+   signed_response, err := protobuf.Unmarshal(response)
+   if err != nil {
+      return nil, err
+   }
+   session_key, err := signed_response.Get_Bytes(4)
+   if err != nil {
+      return nil, err
+   }
+   key, err := rsa.DecryptOAEP(sha1.New(), nil, m.private_key, session_key, nil)
+   if err != nil {
+      return nil, err
+   }
+   // message
+   var buf []byte
+   buf = append(buf, 1)
+   buf = append(buf, "ENCRYPTION"...)
+   buf = append(buf, 0)
+   buf = append(buf, m.license_request...)
+   buf = append(buf, 0, 0, 0, 0x80)
+   // CMAC
+   mac, err := cmac.New(aes.NewCipher, key)
+   if err != nil {
+      return nil, err
+   }
+   mac.Write(buf)
+   block, err := aes.NewCipher(mac.Sum(nil))
+   if err != nil {
+      return nil, err
+   }
+   var cons Containers
+   // .Msg.Key
+   for _, message := range signed_response.Get(2).Get_Messages(3) {
+      var con Container
+      iv, err := message.Get_Bytes(2)
+      if err != nil {
+         return nil, err
+      }
+      con.Key, err = message.Get_Bytes(3)
+      if err != nil {
+         return nil, err
+      }
+      con.Type, err = message.Get_Varint(4)
+      if err != nil {
+         return nil, err
+      }
+      cipher.NewCBCDecrypter(block, iv).CryptBlocks(con.Key, con.Key)
+      con.Key = unpad(con.Key)
+      cons = append(cons, con)
+   }
+   return cons, nil
+}
+
+type Poster interface {
+   Request_URL() string
+   Request_Header() http.Header
+   Request_Body([]byte) ([]byte, error)
+   Response_Body([]byte) ([]byte, error)
+}
+
 type no_operation struct{}
 
 func (no_operation) Read(buf []byte) (int, error) {
    return len(buf), nil
-}
-
-func unpad(buf []byte) []byte {
-   if len(buf) >= 1 {
-      pad := buf[len(buf)-1]
-      if len(buf) >= int(pad) {
-         buf = buf[:len(buf)-int(pad)]
-      }
-   }
-   return buf
-}
-
-func Key_ID(raw string) ([]byte, error) {
-   raw = strings.ReplaceAll(raw, "-", "")
-   return hex.DecodeString(raw)
-}
-
-func PSSH_Key_ID(raw string) ([]byte, error) {
-   _, after, ok := strings.Cut(raw, "data:text/plain;base64,")
-   if ok {
-      raw = after
-   }
-   pssh, err := base64.StdEncoding.DecodeString(raw)
-   if err != nil {
-      return nil, err
-   }
-   cenc_header, err := protobuf.Unmarshal(pssh[32:])
-   if err != nil {
-      return nil, err
-   }
-   return cenc_header.Get_Bytes(2)
-}
-
-type Module struct {
-   license_request []byte
-   private_key *rsa.PrivateKey
-}
-
-var Client = http.Default_Client
-
-type Containers []Container
-
-func (c Container) String() string {
-   return hex.EncodeToString(c.Key)
 }
